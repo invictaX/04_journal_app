605353b44da0c8cd0dc53a29c223970c478c8014
invictaX <isaacnickell@gmail.com>
2017-01-03 17:00:19 -0800

    Added code

--- journal.py
+++ journal.py
@@ -2,6 +2,13 @@
 import os
 
 def load(name):
+    """
+    This method creates and loads a new journal.
+    :param name: The base name of the journal to load.
+    :return: A new journal data structure populated with the file data.
+    """
+
+
     data = []
     filename = get_full_pathname(name)
 
